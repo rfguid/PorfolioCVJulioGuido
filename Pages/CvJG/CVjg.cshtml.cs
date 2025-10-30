76ad9a15d302944e4a7d915ec309b85c7e35589b
rfguid <rafaelj10@outlook.com>
2025-10-30 10:34:53 -0600

    Add files via upload

---
+++ Pages/CvJG/CVjg.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortifolioGuiDo.Pages.Projects
+{
+    public class CVjgModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Aquí puedes agregar lógica que se ejecuta al cargar la página
+        }
+    }
+}
