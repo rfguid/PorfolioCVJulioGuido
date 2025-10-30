b93f931fb02909c49b137d0ddb481ad94aa0cd62
rfguid <rafaelj10@outlook.com>
2025-10-30 13:31:55 -0600

    Add files via upload

---
+++ Pages/Projects/ProjectJG.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortifolioGuiDo.Pages.Projects
+{
+    public class ProjectJGModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Aquí puedes agregar lógica que se ejecuta al cargar la página
+        }
+    }
+}
+
